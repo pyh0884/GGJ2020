525a447cf8645ecd862cdec7f73f556b6bd4211d
pyh0884 <pyh2084@hotmail.com>
2020-01-31 23:54:19 +0800

    AudioManager

---
+++ Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    private bool paused = false;
+    public GameObject PauseMenu;
+    public GameObject cursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Menu") && !paused)
+        {
+            Cursor.visible = false;
+
+            Time.timeScale = 0;            
+            PauseMenu.SetActive(true);
+            cursor.SetActive(true);
+            paused = true;
+        }
+        else if ((Input.GetButtonDown("Menu")||Input.GetButtonDown("Cancel")) && paused)
+        {
+            Cursor.visible = false;
+
+            PauseMenu.SetActive(false);
+            cursor.SetActive(false);
+            Time.timeScale = 1;
+            paused = false;
+        }
+    }
+    public void resume()
+    {
+        Cursor.visible = false;
+
+        PauseMenu.SetActive(false);
+        cursor.SetActive(false);
+        Time.timeScale = 1;
+        paused = false;
+    }
+}
---
+++ Assets/Scripts/UI/Trans.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Trans : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public void LoadScene(int number) 
+    {
+        StartCoroutine(LoadSceneFunction(number));      
+    }
+
+    IEnumerator LoadSceneFunction(int number) 
+    {
+
+        if (PauseMenu != null)
+        {
+            PauseMenu.SetActive(false);
+        }
+
+        Time.timeScale = 1;
+        yield return new WaitForSeconds(0.7f);
+        SceneManager.LoadScene(number);
+        yield return new WaitForSeconds (0.7f);
+    }
+
+    public void QuickLoad(int number)
+    {
+        SceneManager.LoadScene(number);
+    }
+
+	public void ClearSaveData() {
+		if (PlayerPrefs.HasKey("MAXHP")) PlayerPrefs.DeleteKey("MAXHP");
+		if (PlayerPrefs.HasKey("CRIT")) PlayerPrefs.DeleteKey("CRIT");
+		if (PlayerPrefs.HasKey("DAMAGE")) PlayerPrefs.DeleteKey("DAMAGE");
+		if (PlayerPrefs.HasKey("YellowBossBeaten"))PlayerPrefs.DeleteKey("YellowBossBeaten");
+		if (PlayerPrefs.HasKey("BlueBossBeaten"))PlayerPrefs.DeleteKey("BlueBossBeaten");
+		if (PlayerPrefs.HasKey("GreenBossBeaten"))PlayerPrefs.DeleteKey("GreenBossBeaten");
+		if (PlayerPrefs.HasKey("RespwanX"))PlayerPrefs.DeleteKey("RespwanX");
+		if (PlayerPrefs.HasKey("RespwanY"))PlayerPrefs.DeleteKey("RespwanY");
+		if (PlayerPrefs.HasKey("RespwanSceneIndex"))PlayerPrefs.DeleteKey("RespwanSceneIndex");
+        if (PlayerPrefs.HasKey("YELLOW")) PlayerPrefs.DeleteKey("YELLOW"); 
+        if (PlayerPrefs.HasKey("GREEN")) PlayerPrefs.DeleteKey("GREEN");
+        if (PlayerPrefs.HasKey("BLUE")) PlayerPrefs.DeleteKey("BLUE");
+        if (PlayerPrefs.HasKey("RED")) PlayerPrefs.DeleteKey("RED");
+        if (PlayerPrefs.HasKey("MONEY")) PlayerPrefs.DeleteKey("MONEY");
+        if (PlayerPrefs.HasKey("YELLOWPOS")) PlayerPrefs.DeleteKey("YELLOWPOS");
+        if (PlayerPrefs.HasKey("GREENPOS")) PlayerPrefs.DeleteKey("GREENPOS");
+        if (PlayerPrefs.HasKey("BLUEPOS")) PlayerPrefs.DeleteKey("BLUEPOS");
+        if (PlayerPrefs.HasKey("REDPOS")) PlayerPrefs.DeleteKey("REDPOS");
+
+
+
+        for (int i = 0; i <= 30; i++) {
+			if (PlayerPrefs.HasKey("TresuareChestOpened" + i)) {
+				PlayerPrefs.DeleteKey("TresuareChestOpened" + i);
+			}
+		}
+	}
+}
