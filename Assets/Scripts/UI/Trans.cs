83487326dd7ec27fc17abcf04b550163a334408a
pyh0884 <pyh2084@hotmail.com>
2020-02-01 00:49:50 +0800

    2019.2.19

--- Assets/Scripts/UI/Trans.cs
+++ Assets/Scripts/UI/Trans.cs
@@ -31,32 +31,4 @@ public class Trans : MonoBehaviour
         SceneManager.LoadScene(number);
     }
 
-	public void ClearSaveData() {
-		if (PlayerPrefs.HasKey("MAXHP")) PlayerPrefs.DeleteKey("MAXHP");
-		if (PlayerPrefs.HasKey("CRIT")) PlayerPrefs.DeleteKey("CRIT");
-		if (PlayerPrefs.HasKey("DAMAGE")) PlayerPrefs.DeleteKey("DAMAGE");
-		if (PlayerPrefs.HasKey("YellowBossBeaten"))PlayerPrefs.DeleteKey("YellowBossBeaten");
-		if (PlayerPrefs.HasKey("BlueBossBeaten"))PlayerPrefs.DeleteKey("BlueBossBeaten");
-		if (PlayerPrefs.HasKey("GreenBossBeaten"))PlayerPrefs.DeleteKey("GreenBossBeaten");
-		if (PlayerPrefs.HasKey("RespwanX"))PlayerPrefs.DeleteKey("RespwanX");
-		if (PlayerPrefs.HasKey("RespwanY"))PlayerPrefs.DeleteKey("RespwanY");
-		if (PlayerPrefs.HasKey("RespwanSceneIndex"))PlayerPrefs.DeleteKey("RespwanSceneIndex");
-        if (PlayerPrefs.HasKey("YELLOW")) PlayerPrefs.DeleteKey("YELLOW"); 
-        if (PlayerPrefs.HasKey("GREEN")) PlayerPrefs.DeleteKey("GREEN");
-        if (PlayerPrefs.HasKey("BLUE")) PlayerPrefs.DeleteKey("BLUE");
-        if (PlayerPrefs.HasKey("RED")) PlayerPrefs.DeleteKey("RED");
-        if (PlayerPrefs.HasKey("MONEY")) PlayerPrefs.DeleteKey("MONEY");
-        if (PlayerPrefs.HasKey("YELLOWPOS")) PlayerPrefs.DeleteKey("YELLOWPOS");
-        if (PlayerPrefs.HasKey("GREENPOS")) PlayerPrefs.DeleteKey("GREENPOS");
-        if (PlayerPrefs.HasKey("BLUEPOS")) PlayerPrefs.DeleteKey("BLUEPOS");
-        if (PlayerPrefs.HasKey("REDPOS")) PlayerPrefs.DeleteKey("REDPOS");
-
-
-
-        for (int i = 0; i <= 30; i++) {
-			if (PlayerPrefs.HasKey("TresuareChestOpened" + i)) {
-				PlayerPrefs.DeleteKey("TresuareChestOpened" + i);
-			}
-		}
-	}
 }
