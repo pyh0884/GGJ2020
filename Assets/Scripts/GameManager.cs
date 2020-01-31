1e16067f9397cb1f0137426f80a2019851705190
pyh0884 <pyh2084@hotmail.com>
2020-01-31 23:45:11 +0800

    Simple GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager instance;
+    public GameObject playerPrefab;
+    private GameObject player;
+    public Vector3 spawnPos;
+    private void Awake()
+    {
+
+        if (SceneManager.GetActiveScene().name == "__Main Menu")
+            Destroy(gameObject);
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
     void Start()
     {
-        
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Respawn()
     {
-        
+        float x = PlayerPrefs.GetFloat("RespwanX", 1.5f);
+        float y = PlayerPrefs.GetFloat("RespwanY", -1);
+        int index = 0;
+        spawnPos = new Vector3(x, y);
+        /*		if(SceneManager.GetActiveScene().buildIndex!=index)*/
+        SceneManager.LoadScene(index);
+    }
+    private void Update()
+    {
+        Cursor.visible = false;
+        int sceneNum = SceneManager.GetActiveScene().buildIndex;
+        if (player == null && sceneNum != 0)
+        {
+            player = Instantiate(playerPrefab, spawnPos, new Quaternion());
+        }
     }
 }
+
