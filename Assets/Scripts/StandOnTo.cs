846c08872c9c90ebf6fa116360671804a64a0d49
pyh0884 <pyh2084@hotmail.com>
2020-02-02 01:44:07 +0800

    Fix QTE triggered from below the character

--- Assets/Scripts/StandOnTo.cs
+++ Assets/Scripts/StandOnTo.cs
@@ -39,52 +39,26 @@ public class StandOnTo : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Foot" && collision.transform.position.y < transform.position.y) 
         {
             // only when player is right above it
-            Vector2 me = new Vector2(transform.position.x, transform.position.y + 0.4f);
-            Vector2 aboveMe = new Vector2(me.x, me.y + 0.2f);
+            Vector2 me = new Vector2(transform.position.x, transform.position.y);
+            Vector2 aboveMe = new Vector2(me.x, me.y + 0.3f);
             
 
-            RaycastHit2D hit = Physics2D.Raycast(me, aboveMe);
-            if (hit && hit.collider.tag == "Player")
-            {
-                Debug.Log(hit.collider.gameObject.name);
+            //RaycastHit2D hit = Physics2D.Raycast(me, aboveMe);
+            //Debug.DrawLine(me, aboveMe,Color
+            //    .red);
+            //if (hit && hit.collider.tag == "Foot")
+            //{
+            //    Debug.Log(hit.collider.gameObject.name);
                 isQTE = true;
                 Recorder.isQTEJump = true;
                 timeCnt = Time.unscaledTime;
                 Time.timeScale = 0;
                 // TODO: Show QTE Hint
                 Debug.Log("Press Jump to Jump!");
-            }
+            //}
         }
     }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            
-            // 不太懂下面这一坨啥用意 先注释掉了
-            /*
-            if (gameObject.transform.parent.GetComponent<Rigidbody2D>().velocity.x > 0.5f)
-            {
-                player.GetComponent<PlayerController>().Connected = true;
-                player.GetComponent<Rigidbody2D>().velocity = gameObject.transform.parent.GetComponent<Rigidbody2D>().velocity;
-            }
-            */
-            
-        }
-        
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            player.GetComponent<PlayerController>().Connected = false;
-        }
-
-
-    }
-
 }
