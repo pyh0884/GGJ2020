4492e5d6d7fb6377d13bea4b97315d08678c5e96
pyh0884 <pyh2084@hotmail.com>
2020-02-01 20:57:02 +0800

    2019.3

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -99,7 +99,7 @@ public class CharacterController : MonoBehaviour
 
     protected void TryFalling()
     {
-        if (!isGround && playerRigidbody2D.velocity.y <= 0 && !Connected) //||(isGround&&playerRigidbody2D.velocity.y==0))
+        if (!isGround && playerRigidbody2D.velocity.y <= 0 /*&& !Connected*/) //||(isGround&&playerRigidbody2D.velocity.y==0))
         {
             jumping = false;
             anim.SetBool("Fall", true);
