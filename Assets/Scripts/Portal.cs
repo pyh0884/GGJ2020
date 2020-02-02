0ceeb8bd5617b789aa3029fe275836137ab27a5b
pyh0884 <pyh2084@hotmail.com>
2020-02-02 14:26:51 +0800

    1

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : Trans
+{
+    public Animator anim;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            anim.SetTrigger("FadeOut");
+            LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+    }
+
+}
